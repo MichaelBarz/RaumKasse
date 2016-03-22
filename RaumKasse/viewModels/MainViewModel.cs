8639bf10b7a646536bcc893caa9c7b4046a3a614
Michael Barz <mb@barz.de>
2016-03-22 15:31:42 +0100

    code cleaning

--- RaumKasse/viewModels/MainViewModel.cs
+++ RaumKasse/viewModels/MainViewModel.cs
@@ -3,11 +3,8 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace RaumKasse.viewModels
